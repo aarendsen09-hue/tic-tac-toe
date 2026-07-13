4b29ad478ae6586bf48b3db68e8e0964cc92f247
Aurora Arendsen <288135956+aarendsen09-hue@users.noreply.github.com>
2026-07-13 18:15:43 -0400

    Create functional single round

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
 const container = document.querySelector(".container");
 const boxes = document.querySelectorAll(".box");
+const screen = document.querySelector(".screen");
+const below = document.querySelector(".below");
+const restart = document.querySelector(".restart");
 
 const gameboard = (() => {
-    const board = [0,0,0,0,0,0,0,0,0];
+    let board = [0,0,0,0,0,0,0,0,0];
 
     const getBoard = () => board;
+    var isPlayable = true;
 
     boxes.forEach(box => {
         box.addEventListener("click", () => {
@@ -13,62 +17,56 @@ const gameboard = (() => {
         })
     });
 
+    // restart.addEventListener("click", () => {
+
+    // })
+
     const makeMove = (loc) => {
-        let madeMove = false;
-        while (madeMove == false) {
-            if (board[loc] === 0) {
-                board[loc] = displayController.getActivePlayer().token;
-                madeMove = true;
-                // for (const box of boxes) {
-                //     if (box.dataset.index == loc) {
-                //         box.textContent = board[loc];
-                //     }
-                // }
+        if (isPlayable) {
+            if (board[loc] !== 0) {
+                return;
             }
-        }
+            board[loc] = displayController.getActivePlayer().token;
 
-        let row = Math.trunc(loc / 3);
-        let col = (loc % 3) - 1;
-        if (loc % 3 == 0) {
-            row = (loc / 3) - 1;
-            col = 2;
-        }
-        var count = 0;
-        for (let i = (loc - col); i < (loc - col) + 3; i++) {
-            if (board[i] !== displayController.getActivePlayer().token) {
-                break;
-            }
-            count++;
-        }
-        if (count == 3) {
-            return displayController.endGame(displayController.getActivePlayer());
-        }
-        count = 0;
-        for (let i = (loc - row); i < (loc - row) + 3; i++) {
-            if (board[i] !== getActivePlayer.token) {
-                break;
-            }
-            count++;
-        }
-        if (count == 3) {
-            return displayController.endGame(displayController.getActivePlayer());
-        }
-        count = 0;
-        if (board[4] == getActivePlayer.token) {
-            if ((board[0] == getActivePlayer().token && board[8] == getActivePlayer().token) || (board[2] == getActivePlayer().token && board[6] == getActivePlayer().token)) {
-                return displayController.endGame(displayController.getActivePlayer());
-            }
-        }
-
-        for (let i = 0; i < 9; i++) {
-            if (board[i] !== 0) {
+            let row = Math.trunc(loc / 3);
+            let col = (loc % 3);
+            var count = 0;
+            for (let i = row*3; i < row*3 + 3; i++) {
+                if (board[i] !== displayController.getActivePlayer().token) {
+                    break;
+                }
                 count++;
             }
-        }
-        if (count == 9) {
-            return displayController.endGame("tie");
-        }
-        displayController.switchPlayerTurn();
+            if (count == 3) {
+                return displayController.endGame(displayController.getActivePlayer());
+            }
+            count = 0;
+            for (let i = col; i < 9; i+=3) {
+                if (board[i] !== displayController.getActivePlayer().token) {
+                    break;
+                }
+                count++;
+            }
+            if (count == 3) {
+                return displayController.endGame(displayController.getActivePlayer());
+            }
+            count = 0;
+            if (board[4] == displayController.getActivePlayer().token) {
+                if ((board[0] == displayController.getActivePlayer().token && board[8] == displayController.getActivePlayer().token) || (board[2] == displayController.getActivePlayer().token && board[6] == displayController.getActivePlayer().token)) {
+                    return displayController.endGame(displayController.getActivePlayer());
+                }
+            }
+
+            for (let i = 0; i < 9; i++) {
+                if (board[i] !== 0) {
+                    count++;
+                }
+            }
+            if (count == 9) {
+                return displayController.endGame("tie");
+            }
+            displayController.switchPlayerTurn();
+    }
     }
 
     const renderBoard = () => {
@@ -77,17 +75,25 @@ const gameboard = (() => {
         });
     }
 
+    const clearBoard = () => {
+        // board = [0,0,0,0,0,0,0,0,0];
+        isPlayable = false;
+    //     boxes.forEach(box => {
+    //         box.textContent = "";
+    // });
+    }
 
     return {
         getBoard,
         makeMove,
         renderBoard,
+        clearBoard,
     };
 })();
 
 const displayController = (() => {
-    playerOneName = "Player One";
-    playerTwoName = "Player Two";
+    const playerOneName = "Player One";
+    const playerTwoName = "Player Two";
 
     const players = [
         {
@@ -115,10 +121,11 @@ const displayController = (() => {
             result.textContent = "Tie!";
         }
         else {
-            result.textContent = `${player} won!`;
+            result.textContent = `${player.name} won!`;
         }
-        container.appendChild(result);
-
+        below.appendChild(result);
+        gameboard.clearBoard();
+        
     }
 
     return {
